24b4f7447a0d82f2b60e5fce973b4f4c506ff7fd
João Pedro Barbosa <jpdsbarbosa@inf.ufpel.edu.br>
2024-09-20 16:03:42 -0300

    feat: up client and configs

---
+++ client/client.go
@@ -0,0 +1,25 @@
+package client
+
+import (
+	"net/http"
+	"noxpay-client/config"
+	"time"
+)
+
+// Client: estutura que mantém o cliente HTTP e configurações para fazer requisições
+type Client struct {
+	HTTPClient *http.Client // Cliente HTTP para fazer requisições
+}
+
+// New: cria e retorna uma nova instância do cliente configurada
+func New() *Client {
+	return &Client{
+		HTTPClient: &http.Client{Timeout: 10 * time.Second}, // timeout de 10 segundos para as requisições
+	}
+}
+
+// DoRequest: executa uma requisição HTTP preparada e adiciona o header da APIKey
+func (c *Client) DoRequest(req *http.Request) (*http.Response, error) {
+	req.Header.Set("api-key", config.APIKey)
+	return c.HTTPClient.Do(req) // Executa a requisição e retorna a resposta
+}
---
+++ config/config.go
@@ -0,0 +1,6 @@
+package config
+
+const (
+	APIKey  = "e82b20ef-0040-42ef-af60-f069eda3c650" // Sua chave de API para autenticação.
+	BaseURL = "https://api2.noxpay.io"               // URL base da API da NoxPay.
+)
