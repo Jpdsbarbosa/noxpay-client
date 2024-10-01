4defcd5fb41f1a3cdd22d7a4e58ed9bd8258aea9
João Pedro Barbosa <jpdsbarbosa@inf.ufpel.edu.br>
2024-10-01 15:54:50 -0300

    feat: arquivo para inicializar o servidor local e rodas as API'S

---
+++ cmd/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/Jpdsbarbosa/noxpay-client/pkg/handlers"
+)
+
+func main() {
+
+	http.HandleFunc("/payment/pix", handlers.CreatePaymentHandler)                  // Para criar um pagamento por pix
+	http.HandleFunc("/payment/", handlers.ConsultPaymentHandler)                    // Para consultar um pagamento
+	http.HandleFunc("/payment/creditcard", handlers.CreateCreditCardPaymentHandler) // Para criar um pagamento por cartão de crédito
+	http.HandleFunc("/account-data", handlers.GetAccountDataHandler)                // Para obter dados da conta
+
+	log.Println("Servidor iniciado na porta 8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("Erro ao iniciar o servidor: ", err)
+	}
+}
